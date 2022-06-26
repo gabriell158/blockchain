ac9f39547b2603c0871f53180d1830b5dd2ddaab
Gabriel <gabriel@alexandria.solar>
2022-06-26 12:10:05 -0300

    .

--- selector/src/index.js
+++ selector/src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.json({ limit: "50000000000mb" }));
+app.use(express.json());
 
 app.get("/", (req, res) => res.json({ message: "hello world" }));
 
